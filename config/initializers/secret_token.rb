49c70c51663629ae02eb05061d27c41c67357cf0
rails --version <test>
2013-06-28 18:12:55 -0500

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+First::Application.config.secret_token = '3943c330a6056602e506262ae85296523f219709b48fb33f12ea0887695a2d690c460ea3cddd48b03ea17582981b8c3d5dcee6f32cb673b09d6f794534ad79e5'
